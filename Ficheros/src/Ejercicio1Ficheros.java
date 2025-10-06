b227636734718f7ceee9539349171babedd4f397
caarmeencv <carmencv253@gmail.com>
2025-10-06 10:01:50 +0200

    ficheros

--- Ficheros/src/Ejercicio1Ficheros.java
+++ Ficheros/src/Ejercicio1Ficheros.java
@@ -1,23 +1,31 @@
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class App {
+public class Ejercicio1Ficheros {
     public static void main(String[] args) throws Exception {
-        Path path = Path.of("D:\\carmen\\AD\\Ficheros\\archivo.txt");
+
+        System.out.println("EJERCICIO 14.1");
+
+        //Path path = Path.of("D:\\carmen\\AD\\Ficheros\\archivo.txt");
 
         //BufferedReader bf = new BufferedReader(new InputStreamReader(new InputStream("archivo")));
 
-        try (BufferedWriter bu = Files.newBufferedWriter(path, StandardCharsets.UTF_8)){
+        System.out.println("Escribir en un archivo txt aaaññññaa");
+
+        try (BufferedWriter bu = Files.newBufferedWriter(Path.of("D:\\carmen\\AD\\Ficheros\\archivo.txt"), StandardCharsets.UTF_8)){
             
-            bu.newLine();
+            bu.write("Línea con la letra ñ");
 
         } catch (Exception e) {
+
         }
 
+        System.out.println("Fin del programa");
+
+        /*
         try (BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
             String linea;
             while ((linea = bufferedReader.readLine()) != null) { 
@@ -26,5 +34,6 @@ public class App {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        */
     }
 }
---
+++ Ficheros/src/Ejercicio2Ficheros.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Ejercicio2Ficheros {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("EJERCICIO 14.1");
+
+        //Path path = Path.of("D:\\carmen\\AD\\Ficheros\\archivo.txt");
+
+        //BufferedReader bf = new BufferedReader(new InputStreamReader(new InputStream("archivo")));
+
+        System.out.println("Escribir en un archivo txt una linea con ñ y otra linea");
+
+        try (BufferedWriter bu = Files.newBufferedWriter(Path.of("D:\\carmen\\AD\\Ficheros\\archivo2.txt"), StandardCharsets.UTF_8)){
+            
+            bu.write("Linea con la letra ñ");
+            bu.flush();
+            
+            bu.newLine();
+            bu.write("Otra línea");
+
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("Dale a enter para escribir");
+            teclado.nextLine();
+            
+
+        } catch (Exception e) {
+
+        }
+
+        System.out.println("Fin del programa");
+
+        /*
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
+            String linea;
+            while ((linea = bufferedReader.readLine()) != null) { 
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+    }
+}
